10c2e0495e5872da5266e0a5e8a4a62c5135cf62
Austin Puckett <puckett_austin@yahoo.com>
2018-09-18 06:40:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,12 +21,12 @@ function destructivelyRemoveFirstKitten() {
 }
 
 function appendKitten(name) {
-  kittens2 = [...kittens, name];
+  const kittens2 = [...kittens, name];
   return kittens2;
 }
 
 function prependKitten(name) {
-  kittens2 = [name, ...kittens];
+  const kittens2 = [name, ...kittens];
   return kittens2;
 }
 
